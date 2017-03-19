c406d1d22c7e497c122de58c9109eff071f6bd2f
drumnation <davidmieloch@gmail.com>
2017-03-18 22:32:48 -0400

    Changes to the opening welcome

--- the-golden-hammer.rb
+++ the-golden-hammer.rb
@@ -78,6 +78,7 @@ end
 def golden_hammer_cli
 
   def greetings
+    print "\e[8;10;61t"
     puts "-----------------------------------------------------------"
     puts "               GREETINGS FELLOW FLATIRONERS!               "
     puts "-----------------------------------------------------------"
@@ -85,6 +86,8 @@ def golden_hammer_cli
 
   def welcome
     welcome_message = %q(
+PREFACE:
+
 The gifted entrepreneur Steve Jobs made some controversial
 comments about innovation during his career. He expressed
 strong agreement with the following aphorism which he
@@ -92,35 +95,12 @@ ascribed to the famous painter Pablo Picasso:
 
 'Good artists copy; great artists steal.'
 
-I would happen to agree. With a B.A. in Music Composition, I
-learned all about transforming any theme into something new.
-Doing this requires many techniques for manipulating music
-in a controlled manner, allowing you to achieve the exact
-aesthetic you hear in your mind. A score is a program that
-runs live humans in synchronization instead of a computer.
+I continue this discussion on my blog-> drumnation.github.io
 
-I spent hours in the music library poring over orchestral
-scores while listening to their recordings. It taught me to
-connect what things sound like with what things look like on
-the page. It also gave me the opportunity to collect a grab
-bag of techniques that I liked a lot and could later use in
-my own music.
+INSTRUCTIONS:
 
-The ultimate secret of master makers is that nobody is
-original. The greatest artists copy as many of their idols
-as they can because they absolutely love them and their
-resulting products often become beautiful diverse blends of
-others' fresh and original ideas, neatly curated by the
-artist.
-
-Every answer to a Flatiron question becomes a tool we will
-use to make our future programs better so why not learn ALL
-the answers? In the spirit of our recent reading assignment
-'Hackers and Painters' I want to give this tool to
-everybody so we can better learn from each other.
-
-When I'm stumped I fire up the Golden Hammer make a list of
-the 5 most cleverly diverse solutions and try to refactor
+When I'm stumped I fire up the Golden Hammer, make a list of
+the 5 most cleverly diverse solutions, and try to refactor
 them into an über answer.
 
 I then take screenshots of my über answer and the selected 5
@@ -129,17 +109,9 @@ also explain that on my blog soon). I look up the docs for,
 and screenshot, any syntax or terms used that I didn't
 understand.
 
-I wrote this program while I was doing my prework and
-decided to clean it up and make it easy for other people to
-use. I updated it with this menu stuff at the end of my first
-week.  I credit this to the smarts of the 0217 class because
-I've been all over their answers and they have some really
-smart people.
+TAKE AWAYS:
 
-My percussion instructor taught us to never practice
-mistakes. Always slow the metronome down until you can play
-it perfect or you're just teaching yourself to play it
-wrong.  Without our community of ideas you could be
+Without our community of ideas you could be
 practicing the longest way to solve your problems exercise
 after exercise and never know it.
 
@@ -157,7 +129,7 @@ Ask me for help anytime! Thanks guys! -Dave Mieloch
 
 ---- Press C to Continue or Q to  ---)
     puts `clear`
-    print "\e[8;68;61t" # resizes window to 55x80
+    print "\e[8;50;61t" # resizes window to 55x80
     puts welcome_message
     case gets.strip.upcase
     when "C"
